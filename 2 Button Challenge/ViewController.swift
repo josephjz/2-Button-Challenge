0bc8c1c90a81bf1d5342bf34fb05b77a8c16ced2
Jennifer Joseph <josephjz@bc.edu>
2020-09-06 11:37:14 -0400

    Code Updated for Chapter 2 Section 4 Challenge Exercise where we explore label object attributes and dot notation for modifying objects within .swift code.

--- 2 Button Challenge/ViewController.swift
+++ 2 Button Challenge/ViewController.swift
@@ -19,11 +19,15 @@ class ViewController: UIViewController {
     
     @IBAction func option1ButtonPressed(_ sender: UIButton) {
         complimentLabel.text = "You are awesome!"
+        complimentLabel.textColor = UIColor.blue
+        complimentLabel.textAlignment = .left
         print("You are awesome! printed")
     }
     
     @IBAction func option2ButtonPressed(_ sender: UIButton) {
         complimentLabel.text = "You are great!"
+        complimentLabel.textAlignment = .right
+        complimentLabel.textColor = UIColor.systemRed
         print("You are great! printed")
     }
 }
